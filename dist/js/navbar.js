90231148e397c92e2e6519a1404d4fafaae550b9
Dzakus <Dzakus13@gmail.com>
2016-06-27 23:03:00 +0200

    Travis integration for publish docs

--- dist/js/navbar.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function iife($) {
-  $('.sowp-navbar__toggle').click(function toggleClick(ev) {
-    ev.preventDefault();
-    var navbar = $(this).closest('.sowp-navbar');
-    navbar.toggleClass('is-expanded');
-  });
-})(jQuery);
