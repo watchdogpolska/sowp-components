1461dc9c8ee8b0945c7c47309407bcdd81cb4296
Dzakus <Dzakus13@gmail.com>
2016-06-30 16:42:37 +0200

    Fix problem related to masonry and image

--- assets/javascript/grid.js
+++ assets/javascript/grid.js
@@ -1,8 +1,9 @@
 (function iife($) {
-  $('.sowp-grid').masonry({
+  const $grid = $('.sowp-grid').masonry({
     itemSelector: '.sowp-grid--item',
     columnWidth: '.sowp-grid--item',
     gutter: '.sowp-grid--gutter',
     percentPosition: true,
   });
+  $grid.imagesLoaded().progress(() => $grid.masonry('layout'));
 }(jQuery));
