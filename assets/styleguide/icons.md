7fd3ee8096c18ecb4142438e7d96c9fdf7f45b28
mik-laj <kamil.bregula@siecobywatelska.pl>
2017-02-05 17:52:05 +0100

    Add a social icon, extract icons code, simplify gulp file

---
+++ assets/styleguide/icons.md
@@ -0,0 +1,136 @@
+# Jak używać?
+
+
+## Icons
+
+
+<table id="icon-table">
+<tr>
+<td>
+    <div class="preview">
+      <i class="isowp-accessibility"></i>
+    </div>
+```
+<i class="isowp-accessibility"></i>
+```
+</td>
+<td>
+    <div class="preview">
+      <i class="isowp-arrow"></i>
+    </div>
+```
+<i class="isowp-arrow"></i>
+```
+</td>
+<td>
+    <div class="preview">
+      <i class="isowp-circle-facebook"></i>
+    </div>
+```
+<i class="isowp-circle-facebook"></i>
+```
+</td>
+<td>
+    <div class="preview">
+      <i class="isowp-circle-instagram"></i>
+    </div>
+```
+<i class="isowp-circle-instagram"></i>
+```
+</td>
+<td>
+    <div class="preview">
+      <i class="isowp-circle-left"></i>
+    </div>
+```
+<i class="isowp-circle-left"></i>
+```
+</td>
+</tr>
+<tr>
+<td>
+    <div class="preview">
+      <i class="isowp-circle-right"></i>
+    </div>
+```
+<i class="isowp-circle-right"></i>
+```
+</td>
+<td>
+    <div class="preview">
+      <i class="isowp-circle-twitter"></i>
+    </div>
+```
+<i class="isowp-circle-twitter"></i>
+```
+</td>
+<td>
+    <div class="preview">
+      <i class="isowp-circle-youtube"></i>
+    </div>
+```
+<i class="isowp-circle-youtube"></i>
+```
+</td>
+<td>
+    <div class="preview">
+      <i class="isowp-clock-15min"></i>
+    </div>
+```
+<i class="isowp-clock-15min"></i>
+```
+</td>
+<td>
+    <div class="preview">
+      <i class="isowp-clock-1h"></i>
+    </div>
+```
+<i class="isowp-clock-1h"></i>
+```
+</td>
+</tr>
+<tr>
+<td>
+    <div class="preview">
+      <i class="isowp-clock-5min"></i>
+    </div>
+```
+<i class="isowp-clock-5min"></i>
+```
+</td>
+<td>
+    <div class="preview">
+      <i class="isowp-clock-more"></i>
+    </div>
+```
+<i class="isowp-clock-more"></i>
+```
+</td>
+<td>
+    <div class="preview">
+      <i class="isowp-docs"></i>
+    </div>
+```
+<i class="isowp-docs"></i>
+```
+</td>
+<td>
+    <div class="preview">
+      <i class="isowp-kreski"></i>
+    </div>
+```
+<i class="isowp-kreski"></i>
+```
+</td>
+<td>
+    <div class="preview">
+      <i class="isowp-printer"></i>
+    </div>
+```
+<i class="isowp-printer"></i>
+```
+</td>
+</tr>
+</table>
+
+
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -17,30 +17,12 @@ const $ = gulpLoadPlugins();
 const bs = browserSync.create();
 const reload = bs.stream;
 
-gulp.task('scss', ['scss:components', 'scss:bootstrap']);
+// SCSS
+gulp.task('scss', ['scss:icons', 'scss:components', 'scss:bootstrap']);
 
-gulp.task('scss:components', ['scss-settings', 'scss-lint'], () =>
-  gulp.src([
-    './assets/stylesheets/_sowp.scss',
-  ])
-      .pipe($.rename({ basename: 'sowp-components' }))
-      .pipe($.if(!isCi, $.plumber()))
-      .pipe($.sass())
-      .pipe($.postcss([autoprefixer]))
-      .pipe(gulp.dest('./dist/css/'))
-      .pipe(reload())
-      .pipe($.postcss([cssnano]))
-      .pipe($.rename({ extname: '.min.css' }))
-      .pipe($.if(!isCi, $.plumber.stop()))
-      .pipe(gulp.dest('./dist/css/'))
-      .pipe(reload())
-);
-
-gulp.task('scss:bootstrap', ['scss-settings', 'scss-lint'], () =>
-  gulp.src([
-    './assets/stylesheets/_sowp-bootstrap.scss',
-  ])
-      .pipe($.rename({ basename: 'sowp-bootstrap' }))
+function scss(file, basename) {
+  return gulp.src([file])
+      .pipe($.rename({ basename }))
       .pipe($.if(!isCi, $.plumber()))
       .pipe($.sass({
         includePaths: ['./node_modules'],
@@ -52,7 +34,19 @@ gulp.task('scss:bootstrap', ['scss-settings', 'scss-lint'], () =>
       .pipe($.rename({ extname: '.min.css' }))
       .pipe($.if(!isCi, $.plumber.stop()))
       .pipe(gulp.dest('./dist/css/'))
-      .pipe(reload())
+      .pipe(reload());
+}
+
+gulp.task('scss:components', ['scss-settings', 'scss-lint'], () =>
+  scss('./assets/stylesheets/_sowp.scss', 'sowp-components')
+);
+
+gulp.task('scss:bootstrap', ['scss-settings', 'scss-lint'], () =>
+  scss('./assets/stylesheets/_sowp-bootstrap.scss', 'sowp-bootstrap')
+);
+
+gulp.task('scss:icons', ['iconfont-gen'], () =>
+  scss('./assets/stylesheets/_sowp-icons.scss', 'sowp-icons')
 );
 
 gulp.task('scss-lint', () =>
@@ -70,6 +64,90 @@ gulp.task('scss-settings', () => {
   });
 });
 
+// Javascript
+gulp.task('js', ['js:single', 'js:combined']);
+
+function js(concat) {
+  return gulp.src('./assets/javascript/**/*.js')
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+    .pipe($.babel())
+    .pipe($.if(concat, $.concat('sowp-components.js')))
+    .pipe(gulp.dest('./dist/js/'))
+    .pipe(reload())
+    .pipe($.sourcemaps.init())
+    .pipe($.uglify())
+    .pipe($.rename({ extname: '.min.js' }))
+    .pipe($.sourcemaps.write('.'))
+    .pipe(gulp.dest('./dist/js/'))
+    .pipe(reload());
+}
+
+gulp.task('js:single', ['js-lint'], () =>
+  js(false)
+);
+
+gulp.task('js:combined', ['js-lint'], () =>
+  js(true)
+);
+
+gulp.task('js-lint', () =>
+  gulp.src('./assets/javascript/**/*.js')
+      .pipe($.eslint())
+);
+
+// Icons
+gulp.task('iconfont-gen', () => {
+  const fontName = 'sowp';
+  return gulp.src(['./assets/icons/*.svg'])
+    .pipe($.iconfontCss({
+      fontName,
+      path: 'scss',
+      targetPath: '../stylesheets/sowp-icons/_icons_generated.scss',
+      fontPath: '../fonts/',
+      cssClass: 'isowp',
+    }))
+    .pipe($.iconfont({
+      fontName,
+      formats: ['svg', 'ttf', 'eot', 'woff', 'woff2'],
+      normalize: true,
+    }))
+    .pipe(gulp.dest('./assets/fonts/'));
+});
+
+gulp.task('iconfont', () => gulp.src('./assets/fonts/*').pipe(gulp.dest('./dist/fonts/')));
+
+
+// Style guide
+function styleguide(input, output, cb) {
+  sherpa(input, {
+    output,
+    template: './assets/styleguide/template.html',
+  }, () => {
+    bs.reload();
+    cb();
+  });
+}
+
+gulp.task('styleguide', ['styleguide:bootstrap', 'styleguide:components', 'styleguide:icons']);
+
+gulp.task('styleguide:components', (done) =>
+  styleguide('./assets/styleguide/index.md', './dist/index.html', done)
+);
+
+gulp.task('styleguide:bootstrap', (done) =>
+  styleguide('./assets/styleguide/bootstrap.md', './dist/bootstrap.html', done)
+);
+
+gulp.task('styleguide:icons', (done) =>
+  styleguide('./assets/styleguide/icons.md', './dist/icons.html', done)
+);
+
+
+// Clean
+gulp.task('clean', () => del(['./dist/**/*']));
+
+// Server
 gulp.task('server', () => {
   bs.init({
     server: {
@@ -78,98 +156,19 @@ gulp.task('server', () => {
   });
 });
 
-gulp.task('js', ['js:single', 'js:combined']);
-
-gulp.task('js:single', ['js-lint'], () =>
-  gulp.src('./assets/javascript/**/*.js')
-    .pipe($.plumber())
-    .pipe($.sourcemaps.init())
-    .pipe($.babel())
-    .pipe(gulp.dest('./dist/js/'))
-    .pipe(reload())
-    .pipe($.sourcemaps.init())
-    .pipe($.uglify())
-    .pipe($.rename({ extname: '.min.js' }))
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/js/'))
-    .pipe(reload())
-);
-
-gulp.task('js:combined', ['js-lint'], () =>
-  gulp.src('./assets/javascript/**/*.js')
-    .pipe($.plumber())
-    .pipe($.sourcemaps.init())
-    .pipe($.babel())
-    .pipe($.concat('sowp-components.js'))
-    .pipe(gulp.dest('./dist/js/'))
-    .pipe(reload())
-    .pipe($.sourcemaps.init())
-    .pipe($.uglify())
-    .pipe($.rename({ extname: '.min.js' }))
-    .pipe($.sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/js/'))
-    .pipe(reload())
-);
-
-gulp.task('js-lint', () =>
-  gulp.src('./assets/javascript/**/*.js')
-      .pipe($.eslint())
-);
-
-gulp.task('iconfont-gen', () => {
-  const fontName = 'sowp';
-  return gulp.src(['./assets/icons/*.svg'])
-    .pipe($.iconfontCss({
-      fontName,
-      path: 'scss',
-      targetPath: '../stylesheets/sowp/_icons_generated.scss',
-      fontPath: '../fonts/',
-      cssClass: 'isowp'
-    }))
-    .pipe($.iconfont({
-      fontName,
-      formats: ['svg', 'ttf', 'eot', 'woff', 'woff2'],
-      normalize: true
-    }))
-    .pipe(gulp.dest('./assets/fonts/'));
-});
-
-gulp.task('iconfont', () => gulp.src('./assets/fonts/*').pipe(gulp.dest('./dist/fonts/')));
-
-
-gulp.task('styleguide', ['styleguide:bootstrap', 'styleguide:components']);
-
-gulp.task('styleguide:components', (done) =>
-  sherpa('./assets/styleguide/index.md', {
-    output: './dist/index.html',
-    template: './assets/styleguide/template.html',
-  }, () => {
-    bs.reload();
-    done();
-  })
-);
-
-gulp.task('styleguide:bootstrap', (done) =>
-  sherpa('./assets/styleguide/bootstrap.md', {
-    output: './dist/bootstrap.html',
-    template: './assets/styleguide/template.html',
-  }, () => {
-    bs.reload();
-    done();
-  })
-);
-
-
-gulp.task('clean', () => del(['./dist/**/*']));
-
+// Watch changes
 gulp.task('watch', () => {
   gulp.watch('./assets/stylesheets/**/*.scss', ['scss']);
-  gulp.watch('./assets/icons/**/*.svg', cb => runSequence('iconfont-gen', 'scss'));
+  gulp.watch('./assets/icons/**/*.svg', () => runSequence(
+    'iconfont-gen',
+    'styleguide:icons',
+    'scss:icons'));
   gulp.watch('./assets/javascript/**/*.js', ['js']);
   gulp.watch('./assets/styleguide/**/*', ['styleguide']);
   gulp.watch('package.json', ['build']);
 });
 
+// Run all task in sequences
 gulp.task('build', cb =>
   runSequence(
     'clean',
